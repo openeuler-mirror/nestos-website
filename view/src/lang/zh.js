0ecca29bf3c3ebd001d1fdbf043bb882b9e7d223
duyiwei <duyiwei@kylinos.cn>
2023-06-27 14:45:25 +0800

    update 22.03-LTS-SP2 download URL and add openstack ISO

--- view/src/lang/zh.js
+++ view/src/lang/zh.js
@@ -8,7 +8,7 @@ export default {
     nestosTitle: '开启NestOS之旅',
     downloadTitle: '获取NestOS资源',
     issueTitle: '我想对NestOS说',
-    description: 'NestOS云底座操作系统，集成了rpm-ostree支持、ignition配置等技术，采用双系分区、原子化更新的设计思路，使用nestos-assembler快速集成构建。并针对K8S、openStack、OKD等平台进行适配，优化容器运行底噪，与centos8相比容器启动速度提升了30%，容器创建速度提升了60%，适合作为以容器化应用为主的云场景基础运行环境。',
+    description: 'NestOS云底座操作系统，集成了rpm-ostree支持、ignition配置等技术，采用双根文件系统、原子化更新的设计思路，使用nestos-assembler快速集成构建。并针对K8S、openStack、OKD等平台进行适配，优化容器运行底噪，与centos8相比容器启动速度提升了30%，容器创建速度提升了60%，适合作为以容器化应用为主的云场景基础运行环境。',
     mainDesc: '目前NestOS已发布23.03版本，欢迎大家点击上方【获取NestOS资源】下载体验。',
   },
   minititle: {
@@ -28,12 +28,12 @@ export default {
     title2: '简单易用的安装配置过程',
     title3: '安全可靠的包管理方式',
     title4: '友好可控的自动更新机制',
-    title5: '紧密配合的双系统分区',
+    title5: '紧密配合的双根文件系统',
     character1: 'NestOS集成适配了iSulad、Docker、Podman等主流容器引擎，为用户提供轻量级、定制化的云场景OS。',
     character2: 'NestOS采用ignition技术，可以以相同的配置方便地完成大批量集群结点的安装配置工作。',
     character3: 'NestOS使用rpm-ostree进行软件包管理，搭配openEuler软件包源，确保原子化更新的安全稳定状态。',
     character4: 'NestOS使用zincati提供自动更新服务，可实现节点自动更新与重新引导，实现集群节点有序升级而服务不中断。',
-    character5: 'NestOS采用双系统分区设计，确保NestOS运行期间的完整性与安全性，并计划与社区Kubeos融合，各取所长。',
+    character5: 'NestOS采用双根文件系统设计，确保NestOS运行期间的完整性与安全性，并计划与社区Kubeos融合，各取所长。',
   },
   compare: {
     desc: '使用 NestOS beta 版本横向对比 openEuler21.09、openEuler20.03LTS、Centos8 系统运行 iSulad2.0.10 容器引擎性能。',
